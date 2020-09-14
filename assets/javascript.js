c88c8295e208f9f2706bdfd8add72532fc77a6bc
Melanie Hall <hallmt313@gmail.com>
2020-09-13 21:41:04 -0400

    Answer texts populates to buttons, highscore page exists

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,12 +1,18 @@
 var quizQuestion = document.querySelector("#quiz-question");
-var quizAnswers = document.querySelector("#answer");
-var answerButtons = document.querySelector("#answer-buttons");
+var answer0 = document.querySelector("#answer0");
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
 var questResult = document.querySelector("#result");
+var timer = document.querySelector("#timertext");
+
+var buttons = document.querySelector(".btn");
 var i = 0;
+var count = 0;
 var theQuestions = [
     //First Question
     {
-        question: "Here the instructions for the quiz will display ",
+        question: "Test your coding knowledge before the time runs out! Get an answer wrong and the time drops by 10 seconds. When you're done, log your high score!",
     },
     //Second Question
     {
@@ -19,15 +25,16 @@ var theQuestions = [
         ],
         correctAnswer: "2. Javascript, HTML, CSS"
     },
+    //Third Question
     {
-        question: "NEW What are the three fundamental programming languages of the modern web?",
+        question: "NEW Question?",
         answers: [
-            "1. C++, HTML, JQuery",
-            "2. Javascript, HTML, CSS",
-            "3. JQuery, HTML, C++",
-            "4. None of the above"
+            "1. Answer",
+            "2. JAnd anotha one",
+            "3. c",
+            "4. d"
         ],
-        correctAnswer: "2. Javascript, HTML, CSS"
+        correctAnswer: ""
     },
 
 ];
@@ -36,27 +43,33 @@ showCard();
 
 //Function iterate through the object theQuestions containing questions and answers
 function showCard(){
-    var questionText = theQuestions[i].question.toString();
+    //var questionText = theQuestions[i].question.toString();
     if (i===0){
+        var questionText = theQuestions[i].question.toString();
         quizQuestion.append(questionText);
-        quizAnswers.innerHTML = "Start!";
+        answer0.innerHTML = "Start!";
+        answer1.style.opacity = 0;
+        answer2.style.opacity = 0;
+        answer3.style.opacity = 0;
         i++;
     }
     else if(i < theQuestions.length){
+        var questionText = theQuestions[i].question.toString();
         quizQuestion.innerHTML = "";
-        quizAnswers.innerHTML = "";   
+        answer0.innerHTML = "";   
+        answer1.style.opacity = 1;
+        answer2.style.opacity = 1;
+        answer3.style.opacity = 1;
         quizQuestion.append(questionText);
         //add attribute btn-index to iterate through buttons and assign answer property
         for(var j = 0; j < theQuestions[i].answers.length; j++){
-            var answerBtn = document.createElement("button");
-            answerBtn.textContent = theQuestions[i].answers[j];
-            console.log(theQuestions[i].answers[j]);
-            answerButtons.appendChild(answerBtn);
+            this["answer"+j].textContent = theQuestions[i].answers[j];
+                       
         };
         i++;
     }
     else {
-        //go to the results page to display high scores
+        window.location.href = "highscores.html";
     }
 
 };
@@ -67,7 +80,7 @@ function showResult(){
 };
 
 //Executes functions to show the next question and the result to the previous question.
-quizAnswers.addEventListener("click", function(event){
+buttons.addEventListener("click", function(event){
     showCard();
     showResult();
 });
