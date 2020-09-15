4a7e52f8345c2e5ef182aedcb281056c438ee0ac
Melanie Hall <hallmt313@gmail.com>
2020-09-15 12:08:43 -0400

    Closer to a working high score page. Ever closer ...

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,22 +1,37 @@
 //Variable to access html elements
 var highSc = document.querySelector("#high-scores");
 var listSc = document.querySelector("#list-scores");
-var lstIt = document.createElement("li");
-//Empty array to store previous high scores
-var runningSc = [];
-
-
 highSc.append("High Scores");
+var scores = [];
+
+//what do we need to do
+//we need to store the high score to other high scores upon load
+//we need to then display the stored high scores in a list
+init();
 storeHS();
-listSc.append(runningSc);
+renderHS();
 
+function init(){
 
-function storeHS (){
-    var listIt = localStorage.getItem("highScore");
-    var userSc = localStorage.getItem("userScore");
-    runningSc.push(listIt + " : " + userSc + " melanpoints");
-    listSc.appendChild(lstIt);
-    lstIt.append(runningSc[runningSc.length-1]);
+    var storedHS = JSON.parse(localStorage.getItem("scores"));
+
+    if(storedHS !== null ){
+        scores = storedHS;
+    }
 }
 
+function storeHS(){
+    var listIt = localStorage.getItem("highScore");
+    var userSc = localStorage.getItem("userScore");
+    scores.push(listIt + " : " + userSc + " melanpoints");
+    localStorage.setItem("scores", JSON.stringify(scores));
+}
 
+function renderHS(){
+    for(var i = 0; i < scores.length; i++){
+        var daScore = scores[i];
+        var liEl = document.createElement("li");
+        liEl.textContent = daScore;
+        listSc.appendChild(liEl);
+    }
+}
